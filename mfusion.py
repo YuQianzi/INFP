915e6f43556593a0a0a923f46da5e8c651efce86
Qianzi Yu <93464943+YuQianzi@users.noreply.github.com>
2025-06-03 13:06:22 +0800

    Add files via upload

---
+++ mfusion.py
@@ -0,0 +1,333 @@
+import torch
+import torch.nn as nn
+
+class TemporalShift(nn.Module):
+    def __init__(self, n_segment=4, n_div=8, direction='left'):
+        super(TemporalShift, self).__init__()
+        self.n_segment = n_segment
+        self.fold_div = n_div
+        self.direction = direction
+
+        print('=> Using fold div: {}'.format(self.fold_div))
+
+    def forward(self, x):
+        x = self.shift(x, self.n_segment, fold_div=self.fold_div, direction=self.direction)
+        return x
+
+    @staticmethod
+    def shift(x, n_segment=4, fold_div=8, direction='left'):
+        bz, nt, h, w = x.size()
+        c = nt // n_segment
+        x = x.view(bz, n_segment, c, h, w)
+
+        fold = c // fold_div
+
+        out = torch.zeros_like(x)
+        if direction == 'left':
+            out[:, :-1, :fold] = x[:, 1:, :fold]
+            out[:, :, fold:] = x[:, :, fold:]
+        elif direction == 'right':
+            out[:, 1:, :fold] = x[:, :-1, :fold]
+            out[:, :, fold:] = x[:, :, fold:]
+        else:
+            out[:, :-1, :fold] = x[:, 1:, :fold]
+            out[:, 1:, fold: 2 * fold] = x[:, :-1, fold: 2 * fold]
+            out[:, :, 2 * fold:] = x[:, :, 2 * fold:]
+
+        return out.view(bz, nt, h, w)
+
+
+class BasicConv(nn.Module):
+    def __init__(self, in_channel, out_channel, kernel_size, stride, bias=True, norm=False, relu=True, transpose=False):
+        super(BasicConv, self).__init__()
+        if bias and norm:
+            bias = False
+
+        padding = kernel_size // 2
+        layers = list()
+        if transpose:
+            padding = kernel_size // 2 - 1
+            layers.append(
+                nn.ConvTranspose2d(in_channel, out_channel, kernel_size, padding=padding, stride=stride, bias=bias))
+        else:
+            layers.append(
+                nn.Conv2d(in_channel, out_channel, kernel_size, padding=padding, stride=stride, bias=bias))
+        if norm:
+            layers.append(nn.BatchNorm2d(out_channel))
+        if relu:
+            layers.append(nn.GELU())
+        self.main = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.main(x)
+
+class ResBlock_Conv(nn.Module):
+    def __init__(self, in_channel, out_channel):
+        super(ResBlock_Conv, self).__init__()
+        self.conv1 = BasicConv(in_channel, out_channel, kernel_size=3, stride=1, relu=True)
+        self.conv2 = BasicConv(out_channel, out_channel, kernel_size=3, stride=1, relu=False)
+        self.trans_layer = BasicConv(out_channel, out_channel, kernel_size=3, stride=1, relu=False)
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.trans_layer(out)
+        out = self.conv2(out)
+        return out + x
+
+
+
+class SpaBlock(nn.Module):
+    def __init__(self, nc):
+        super(SpaBlock, self).__init__()
+        self.block = ResBlock_Conv(in_channel=nc, out_channel=nc)
+
+    def forward(self, x):
+        yy = self.block(x)
+        return yy
+
+
+class SELayer(nn.Module):
+    def __init__(self, channel, reduction=16):
+        super(SELayer, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.fc = nn.Sequential(
+            nn.Linear(channel, channel // reduction, bias=False),
+            nn.ReLU(inplace=True),
+            nn.Linear(channel // reduction, channel, bias=False),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        b, c, _, _ = x.size()
+        y = self.avg_pool(x).view(b, c)
+        y = self.fc(y).view(b, c, 1, 1)
+        return x * y.expand_as(x)
+
+
+class Frequency_Spectrum_Dynamic_Aggregation(nn.Module):
+    def __init__(self, nc):
+        super(Frequency_Spectrum_Dynamic_Aggregation, self).__init__()
+        self.processmag = nn.Sequential(
+            nn.Conv2d(nc, nc, 1, 1, 0),
+            nn.LeakyReLU(0.1, inplace=True),
+            SELayer(channel=nc),
+            nn.Conv2d(nc, nc, 1, 1, 0))
+        self.processpha = nn.Sequential(
+            nn.Conv2d(nc, nc, 1, 1, 0),
+            nn.LeakyReLU(0.1, inplace=True),
+            SELayer(channel=nc),
+            nn.Conv2d(nc, nc, 1, 1, 0))
+
+    def forward(self, x):
+        ori_mag = torch.abs(x)
+        ori_pha = torch.angle(x)
+        mag = self.processmag(ori_mag)
+        mag = ori_mag + mag
+        pha = self.processpha(ori_pha)
+        pha = ori_pha + pha
+        real = mag * torch.cos(pha)
+        imag = mag * torch.sin(pha)
+        x_out = torch.complex(real, imag)
+
+        return x_out
+
+
+class freq_extractor(nn.Module):
+
+    def __init__(self, in_nc):
+        super(freq_extractor, self).__init__()
+        self.spatial_process = SpaBlock(in_nc)
+        self.frequency_process = Frequency_Spectrum_Dynamic_Aggregation(in_nc)
+        self.cat = nn.Conv2d(2 * in_nc, in_nc, 1, 1, 0)
+
+    def forward(self, x):
+        _, _, H, W = x.shape
+        x_freq = torch.fft.rfft2(x, norm='backward')
+        x = self.spatial_process(x)
+        x_freq = self.frequency_process(x_freq)
+        x_freq_spatial = torch.fft.irfft2(x_freq, s=(H, W), norm='backward')
+
+        xcat = torch.cat([x, x_freq_spatial], 1)
+        x_out = self.cat(xcat)
+
+        return x_out
+
+class SELayer(nn.Module):
+    def __init__(self, num_channels, reduction_ratio=8):
+
+        super(SELayer, self).__init__()
+        num_channels_reduced = num_channels // reduction_ratio
+        self.reduction_ratio = reduction_ratio
+        self.fc1 = nn.Linear(num_channels, num_channels_reduced, bias=True)
+        self.fc2 = nn.Linear(num_channels_reduced, num_channels, bias=True)
+        self.relu = nn.ReLU()
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, input_tensor):
+        batch_size, num_channels, H, W = input_tensor.size()
+
+        squeeze_tensor = input_tensor.view(batch_size, num_channels, -1).mean(dim=2)
+
+        # channel excitation
+        fc_out_1 = self.relu(self.fc1(squeeze_tensor))
+        fc_out_2 = self.sigmoid(self.fc2(fc_out_1))
+
+        a, b = squeeze_tensor.size()
+        output_tensor = torch.mul(input_tensor, fc_out_2.view(a, b, 1, 1))
+        return output_tensor
+
+
+# fusion_module implementation
+class fusion_module(nn.Module):
+    def __init__(self, channels, kernel_dim=1, dilation=1, reduction_ratio=8):
+
+        super(fusion_module, self).__init__()
+        self.pad = kernel_dim + dilation
+        self.border_input = kernel_dim + 2*dilation + 1
+
+        self.relu = nn.ReLU()
+        self.se = SELayer(channels, reduction_ratio=reduction_ratio)
+
+        self.conv1 = nn.Conv2d(in_channels=channels,
+                               out_channels=channels,
+                               kernel_size=kernel_dim)
+        self.conv2 = nn.Conv2d(in_channels=channels,
+                               out_channels=channels,
+                               kernel_size=kernel_dim)
+        self.conv3 = nn.Conv2d(in_channels=channels,
+                               out_channels=channels,
+                               kernel_size=kernel_dim)
+        self.conv4 = nn.Conv2d(in_channels=channels,
+                               out_channels=channels,
+                               kernel_size=kernel_dim)
+
+    def forward(self, x):
+        x = F.pad(x, (self.pad, self.pad, self.pad, self.pad), "constant", 0)
+
+        x1 = self.conv1(x[:, :, :-self.border_input, :-self.border_input])
+        x2 = self.conv2(x[:, :, self.border_input:, :-self.border_input])
+        x3 = self.conv3(x[:, :, :-self.border_input, self.border_input:])
+        x4 = self.conv4(x[:, :, self.border_input:, self.border_input:])
+        x = self.relu(x1 + x2 + x3 + x4)
+
+        x = self.se(x)
+        return x
+
+
+class MultiFeatureFusion(nn.Module):
+    def __init__(self, channels, n_segment=4, n_div=8):
+        super(IntegratedFusionModule, self).__init__()
+        self.temporal_shift = TemporalShift(n_segment=n_segment, n_div=n_div)
+        self.freq_extractor = freq_extractor(channels)
+        self.fusion = fusion_module(channels, reduction_ratio=1)
+
+    def forward(self, x):
+        x1 = x
+        x2 = self.temporal_shift(x)
+        x3 = self.freq_extractor(x)
+        out = self.fusion(x1 + x2 + x3)
+        return out
+
+
+class StarReLU(nn.Module):
+    """
+    StarReLU: s * relu(x) ** 2 + b
+    """
+
+    def __init__(self, scale_value=1.0, bias_value=0.0,
+                 scale_learnable=True, bias_learnable=True,
+                 mode=None, inplace=False):
+        super().__init__()
+        self.inplace = inplace
+        self.relu = nn.ReLU(inplace=inplace)
+        self.scale = nn.Parameter(scale_value * torch.ones(1),
+                                  requires_grad=scale_learnable)
+        self.bias = nn.Parameter(bias_value * torch.ones(1),
+                                 requires_grad=bias_learnable)
+
+    def forward(self, x):
+        return self.scale * self.relu(x) ** 2 + self.bias
+
+class Mlp(nn.Module):
+    """ MLP as used in MetaFormer models, eg Transformer, MLP-Mixer, PoolFormer, MetaFormer baslines and related networks.
+    Mostly copied from timm.
+    """
+
+    def __init__(self, dim, mlp_ratio=4, out_features=None, act_layer=StarReLU, drop=0.,
+                 bias=False, **kwargs):
+        super().__init__()
+        in_features = dim
+        out_features = out_features or in_features
+        hidden_features = int(mlp_ratio * in_features)
+        drop_probs = to_2tuple(drop)
+
+        self.fc1 = nn.Linear(in_features, hidden_features, bias=bias)
+        self.act = act_layer()
+        self.drop1 = nn.Dropout(drop_probs[0])
+        self.fc2 = nn.Linear(hidden_features, out_features, bias=bias)
+        self.drop2 = nn.Dropout(drop_probs[1])
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.act(x)
+        x = self.drop1(x)
+        x = self.fc2(x)
+        x = self.drop2(x)
+        return x
+
+
+
+class trajectory_filter(nn.Module):
+
+    def __init__(self, dim, expansion_ratio=2, reweight_expansion_ratio=.25,
+                 act1_layer=StarReLU, act2_layer=nn.Identity,
+                 bias=False, num_filters=4, size=14, weight_resize=False,
+                 **kwargs):
+        super().__init__()
+        size = to_2tuple(size)
+        self.size = size[0]
+        self.filter_size = size[1] // 2 + 1
+        self.num_filters = num_filters
+        self.dim = dim
+        self.med_channels = int(expansion_ratio * dim)
+        self.weight_resize = weight_resize
+        self.pwconv1 = nn.Linear(dim, self.med_channels, bias=bias)
+        self.act1 = act1_layer()
+        self.reweight = Mlp(dim, reweight_expansion_ratio, num_filters * self.med_channels)
+        self.complex_weights = nn.Parameter(
+            torch.randn(self.size, self.filter_size, num_filters, 2,
+                        dtype=torch.float32) * 0.02)
+        self.act2 = act2_layer()
+        self.pwconv2 = nn.Linear(self.med_channels, dim, bias=bias)
+
+        # self.rfconv = RFAConv(256, 256)
+
+    def forward(self, x):
+        B, H, W, _ = x.shape
+
+        routeing = self.reweight(x.mean(dim=(1, 2))).view(B, self.num_filters,
+                                                          -1).softmax(dim=1)
+        x = self.pwconv1(x)
+        x = self.act1(x)
+        x = x.to(torch.float32)
+        x = torch.fft.rfft2(x, dim=(1, 2), norm='ortho')
+
+        if self.weight_resize:
+            complex_weights = resize_complex_weight(self.complex_weights, x.shape[1],
+                                                    x.shape[2])
+            complex_weights = torch.view_as_complex(complex_weights.contiguous())
+        else:
+            complex_weights = torch.view_as_complex(self.complex_weights)
+        routeing = routeing.to(torch.complex64)
+        weight = torch.einsum('bfc,hwf->bhwc', routeing, complex_weights)
+        if self.weight_resize:
+            weight = weight.view(-1, x.shape[1], x.shape[2], self.med_channels)
+        else:
+            weight = weight.view(-1, self.size, self.filter_size, self.med_channels)
+        x = x * weight
+        x = torch.fft.irfft2(x, s=(H, W), dim=(1, 2), norm='ortho')
+
+        x = self.act2(x)
+        x = self.pwconv2(x)
+        # x = self.rfconv(x)
+        return x
